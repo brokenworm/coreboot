0a63deca3f2ce7410906cda786dd8851d7faa898
Matt DeVillier <matt.devillier@gmail.com>
2016-11-23 23:48:47 -0600

    google/beltino (variants): use sane fan speeds/thermal thresholds
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/google/beltino/acpi_tables.c
+++ src/mainboard/google/beltino/acpi_tables.c
@@ -27,8 +27,6 @@ void acpi_create_gnvs(struct global_nvs *gnvs)
 	gnvs->chromeos.vbt2 = ACTIVE_ECFW_RO;
 #endif
 
-	gnvs->f4of = FAN4_THRESHOLD_OFF;
-	gnvs->f4on = FAN4_THRESHOLD_ON;
 	gnvs->f4pw = FAN4_PWM;
 
 	gnvs->f3of = FAN3_THRESHOLD_OFF;
